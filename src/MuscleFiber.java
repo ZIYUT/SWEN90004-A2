06303bb813c4c3383d1939bb2582436cdc49778e
ZIYU TIAN <ziyut@student.unimelb.edu.au>
2023-05-17 16:19:07 +1000

    fiber

--- src/MuscleFiber.java
+++ src/MuscleFiber.java
@@ -1,2 +1,36 @@
+import java.util.Random;
 public class MuscleFiber {
+    private int maxSize = 4;
+    private double fiberSize;
+    public MuscleFiber() {
+        sproutMuscleFibers();
+        Random r = new Random();
+        double d = r.nextDouble() * 0.4;
+
+        fiberSize = (0.2 + d) * maxSize;
+        }
+    private void sproutMuscleFibers(){
+         /** create a normalized distribution of maximum muscle fiber sizes
+         with median dependent on % of slow twitch fibers.
+         */
+        for (int i=0; i <20; i++) {
+            Random r = new Random();
+            int n = r.nextInt(100);
+            if (n > Params.percentageOfSlowTwitchFibers){
+                maxSize ++;
+            }
+        }
+    }
+    private void regulateMuscleFibers(){
+        /** Create a unique starting fiber size.
+         *  Simulate the body's natural limits on minimum and maximum fiber sizes.
+         */
+        Random r = new Random();
+        double d = r.nextDouble() * 0.4;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/Params.java
+++ src/Params.java
@@ -1,2 +1,18 @@
 public class Params {
+    static final public double hormoneDiffuseRate = 0.75;
+    static final public int anabolicHormoneMax = 200;
+    static final public int anabolicHormoneMin = 50;
+    static final public int catabolicHormoneMax = 250;
+    static final public int catabolicHormoneMin = 52;
+    public int anabolicHormone = 50;
+    public int catabolicHormone = 52;
+    static final public int intensity = 100;
+    // Range for intensity should be 50 ~ 100.
+    static final public double sleepHours = 8;
+    // Range for sleepHours should be 0.0 ~ 12.0 hours.
+    static final public int daysBetweenWorkouts = 5;
+    // Range for daysBetweenWorkouts should be 1 ~ 30 days.
+    static final public int percentageOfSlowTwitchFibers = 50;
+    // Range for percentageOfSlowTwitchFibers should be 0 ~ 100.
+
 }
