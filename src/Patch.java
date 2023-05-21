699ef8b42bd5502e57903cb366d41f75a09af49c
YongleChen <110369238+YongleChen@users.noreply.github.com>
2023-05-21 17:06:08 +1000

    test and update diffuse and fixed bugs that loss Hormone during diffuse

--- src/Patch.java
+++ src/Patch.java
@@ -2,80 +2,83 @@ import java.lang.invoke.MutableCallSite;
 import java.util.List;
 
 import java.util.*;
+
 public class Patch {
     public MuscleFiber fiber;
     public int coordinateX;
     public int coordinateY;
     public double anabolicHormone = 50;
     public double catabolicHormone = 52;
-    public Patch(int coordinateX, int coordinateY,MuscleFiber fiber) {
+    public Patch(int coordinateX, int coordinateY, MuscleFiber fiber) {
         this.coordinateX = coordinateX;
         this.coordinateY = coordinateY;
         this.fiber = fiber;
     }
+
     public void performDailyActivity() {
         /**
          * Simulate hormonal effect of lifestyle with the daily activities
          * such as working and sleeping
          */
         this.catabolicHormone = this.catabolicHormone + 2.0 * log(this.fiber.fiberSize, 10);
-        this.anabolicHormone = this.anabolicHormone + 2.5 * log(this.fiber.fiberSize,10);
+        this.anabolicHormone = this.anabolicHormone + 2.5 * log(this.fiber.fiberSize, 10);
 
     }
-    public void sleep(){
+
+    public void sleep() {
         /**
          * Simulate the hormonal effect of sleeping
          */
         this.catabolicHormone = this.catabolicHormone - 0.5 * log(this.catabolicHormone, 10) * Params.sleepHours;
-        this.anabolicHormone = this.anabolicHormone - 0.48 * log(this.anabolicHormone,10) * Params.sleepHours;
+        this.anabolicHormone = this.anabolicHormone - 0.48 * log(this.anabolicHormone, 10) * Params.sleepHours;
     }
 
-    public void developMuscle(){
+    public void developMuscle() {
         /**
          *  Catabolic hormones must prepare the fibers for growth before the
          *  anabolic hormones may add mass to the fibers
          */
         this.fiber.fiberSize = (this.fiber.fiberSize - 0.2 * log(this.catabolicHormone, 10));
-        if(log(this.anabolicHormone, 10)<=(1.05*log(this.catabolicHormone, 10))){
+        if (log(this.anabolicHormone, 10) <= (1.05 * log(this.catabolicHormone, 10))) {
             this.fiber.fiberSize = (this.fiber.fiberSize + 0.2 * log(this.anabolicHormone, 10));
-        }
-        else {
-            this.fiber.fiberSize = (this.fiber.fiberSize + 0.2 * 1.05* log(this.catabolicHormone, 10));
+        } else {
+            this.fiber.fiberSize = (this.fiber.fiberSize + 0.2 * 1.05 * log(this.catabolicHormone, 10));
         }
         this.fiber.regulateMuscleFibers();
     }
 
-    public void liftWeight(){
+    public void liftWeight() {
         /**
          * Simulate hormonal effect of weight lifting
          */
         Random r = new Random();
         int n = r.nextInt(10000);
-        if(n<(Params.intensity*Params.intensity)){
-            this.anabolicHormone = this.anabolicHormone + log(this.fiber.fiberSize, 10)*55;
-            this.catabolicHormone = this.catabolicHormone + log(this.fiber.fiberSize, 10) *44;
+        if (n < (Params.intensity * Params.intensity)) {
+            this.anabolicHormone = this.anabolicHormone + log(this.fiber.fiberSize, 10) * 55;
+            this.catabolicHormone = this.catabolicHormone + log(this.fiber.fiberSize, 10) * 44;
         }
     }
-    public void regulateHormones(){
+
+    public void regulateHormones() {
         /**
          *   Hormones diffuse to neighboring fibers
          *   if there are to many or to few hormones in an area,
          *   the body will try very hard to restore a balance
          */
-        List<Patch> neighbours = getNeighbours(this.coordinateX,this.coordinateY);
+        List<Patch> neighbours = getNeighbours(this.coordinateX, this.coordinateY);
         diffuse(neighbours);
 
         // Set the anabolicHormone and catabolicHormone values in the range of max and min values.
-        if (this.anabolicHormone > Params.anabolicHormoneMax){
+        if (this.anabolicHormone > Params.anabolicHormoneMax) {
             this.anabolicHormone = Params.anabolicHormoneMax;
         }
-        if (this.anabolicHormone < Params.anabolicHormoneMin){
+        if (this.anabolicHormone < Params.anabolicHormoneMin) {
             this.anabolicHormone = Params.anabolicHormoneMin;
         }
-        if (this.catabolicHormone > Params.catabolicHormoneMax){
+        if (this.catabolicHormone > Params.catabolicHormoneMax) {
             this.catabolicHormone = Params.catabolicHormoneMax;
         }
-        if (this.catabolicHormone < Params.catabolicHormoneMin){
+        if (this.catabolicHormone < Params.catabolicHormoneMin) {
             this.catabolicHormone = Params.catabolicHormoneMin;
         }
     }
@@ -86,19 +89,45 @@ public class Patch {
          */
         return Math.log(value) / Math.log(base);
     }
+
     public void diffuse(List<Patch> neighbours) {
+        /**
+         * Implemented diffuse according to the NetLogo Dictionary.
+         *
+         * Assumed the size of the Muscle should more than 2*2,
+         * so the size of neighbours should be 3(conner),5(edge),8(Interior) respectively.
+         *
+         * Tells each patch to give equal shares of (number * 100) percent of the value of patch-variable
+         * to its eight neighboring patches. number(it is from Params.DIFFUSION) should be between 0 and 1.
+         * Regardless of topology the sum of patch-variable will be conserved across the world.
+         * (If a patch has fewer than eight neighbors, each neighbor still gets an eighth share;
+         * the patch keeps any leftover shares.)
+         *
+         * Note that this is an observer command only, even though you might expect it to be a patch command.
+         * (The reason is that it acts on all the patches at once -- patch commands act on individual patches.)
+         */
         double oldAnabolic = this.anabolicHormone;
         double oldCatabolic = this.catabolicHormone;
-        System.out.println(anabolicHormone);
-        this.anabolicHormone *= (1 - Params.DIFFUSION);
-        this.catabolicHormone *= (1 - Params.DIFFUSION);
+        if (neighbours.size() == 3) {
+            this.anabolicHormone *= (1 - Params.DIFFUSION*(3/8));
+            this.catabolicHormone *= (1 - Params.DIFFUSION*(3/8));
+        } else if (neighbours.size() == 5) {
+            this.anabolicHormone *= (1 - Params.DIFFUSION*(5/8));
+            this.catabolicHormone *= (1 - Params.DIFFUSION*(5/8));
+        } else if (neighbours.size() == 8) {
+            this.anabolicHormone *= (1 - Params.DIFFUSION);
+            this.catabolicHormone *= (1 - Params.DIFFUSION);
+        } else {
+            throw new Error("Error in diffuse method, size of the Muscle should more than 2*2, " +
+                    "neighbours should not in the size out of 3,5,8");
+        }
 
         for (Patch p : neighbours) {
             Muscle.patches[p.coordinateX][p.coordinateY].anabolicHormone += (oldAnabolic * Params.DIFFUSION) / 8;
             Muscle.patches[p.coordinateX][p.coordinateY].catabolicHormone += (oldCatabolic * Params.DIFFUSION) / 8;
         }
-        System.out.println(anabolicHormone);
     }
+
     public List<Patch> getNeighbours(int x, int y) {
 
         ArrayList<Patch> neighbours = new ArrayList<Patch>();
